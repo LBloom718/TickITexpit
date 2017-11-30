9d0a74b56150d5d80d2ee06fe465147dc8e7d7b2
LBloom718 <azelinski718@gmail.com>
2017-11-30 18:37:36 -0500

    Committing all code

---
+++ FinalProject/Controllers/ticketsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FinalProject.Infrastructure;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    [Authorize]
+    public class ticketsController : Controller
+    {
+        private FinalProjectDBEntities db = new FinalProjectDBEntities();
+
+        // GET: tickets
+        public ActionResult Index()
+        {
+            //var tickets = db.tickets.Include(t => t.user);
+            //var email = System.Security.Claims.ClaimsPrincipal.Current.Claims.First().Value; 
+            var tickets = db.tickets.Where(t => t.user.email == email);
+            return View(tickets.ToList());
+        }
+
+        // GET: tickets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ticket ticket = db.tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // GET: tickets/Create
+        public ActionResult Create()
+        {
+            ViewBag.userID = new SelectList(db.users, "userID", "firstName");
+            return View();
+        }
+
+        // POST: tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ticketID,userID,date,description,status,type")] ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                var maxValue = db.tickets.Max(t => t.ticketID);
+                ticket.ticketID = maxValue + 1;
+                ticket.status = 1;
+                db.tickets.Add(ticket);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.userID = new SelectList(db.users, "userID", "firstName", ticket.userID);
+            return View(ticket);
+        }
+
+        // GET: tickets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ticket ticket = db.tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.userID = new SelectList(db.users, "userID", "firstName", ticket.userID);
+            return View(ticket);
+        }
+
+        // POST: tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ticketID,userID,date,description,status,type")] ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.userID = new SelectList(db.users, "userID", "firstName", ticket.userID);
+            return View(ticket);
+        }
+
+        // GET: tickets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ticket ticket = db.tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ticket ticket = db.tickets.Find(id);
+            db.tickets.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
